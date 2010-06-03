a9f02e8aaf7832432cfc497a780cea9b8391614e
Ben Heiskell <etherealpanda.mail@gmail.com>
2010-06-03 19:09:22 +0000

    Cleaning up the user model a bit. I'm still not satisfied with the beforeFind hack, but it seems to work correctly for now.

--- app/models/user.php
+++ app/models/user.php
@@ -1,42 +1,64 @@
 <?php
 class User extends AppModel {
-  var $displayField = 'username';
 
   var $name = 'User';
+  var $displayField = 'username';
   var $validate = array(
     'username' => array(
-      'maxlength' => array(
-        'required' => true, 
+      'range' => array(
+        'required'   => true,
         'allowEmpty' => false,
-        'rule' => array('maxlength', 64)
+        'rule'       => array('maxlength', 64),
+        'message'    => 'Username must be between 1 and 64 characters long.'
       ),
       'isUnique' => array(
-        'rule' => 'isUnique',
-        'message' => 'Username is already in use. Please select another.'
+        'rule'       => 'isUnique',
+        'message'    => 'Username is already in use. Please select another.'
       )
     ),
     'password' => array(
-      'required' => true,
-      'rule' => array('between', 8, 128), 
-      'message' => 'Passwords must be at least 8 characters long'
+      'required'     => true,
+      'rule'         => array('between', 8, 128),
+      'message'      => 'Passwords must be at least 8 characters long.'
     ),
     'password_confirm' => array(
-      'required' => true,
-      'rule' => array('comparePassword'),
-      'message' => 'Passwords did not match.'
+      'required'     => true,
+      'rule'         => array('comparePassword'),
+      'message'      => 'Passwords did not match.'
     ),
     'email' => array(
-      'required' => false,
-      'allowEmpty' => true,
-      'rule' => array('maxlength', 320)
+      'maxLength' => array(
+        'rule'       => array('maxlength', 320),
+        'message'    => 'Email must be less than 320 characters long.'
+      ),
+      'email' => array(
+        'rule'       => 'email',
+        'allowEmpty' => true,
+        'message'    => 'Please enter a valid email address.'
+      )
     ),
     'avatar' => array(
-      'maxlength' => array('rule' => array('maxlength', 1024)),
-      'url' => array('allowEmpty' => true, 'rule' => 'url')
+      'maxlength' => array(
+        'rule'       => array('maxlength', 1024),
+        'message'    => 'Avatar URL must be less than 1024 characters long.'
+      ),
+      'url' => array(
+        'rule'       => 'url',
+        'allowEmpty' => true,
+        'message'    => 'Avatar must be a valid URL'
+      )
     )
   );
 
   var $hasMany = array('Character');
+
+  /**
+   * Validator rule used by password_confirm.
+   */
+  function comparePassword() {
+    return $this->data['User']['password']
+        == $this->data['User']['password_confirm'];
+  }
 
   /**
    * Overload hack to prevent the Auth component from automatically hashing
@@ -59,42 +81,37 @@ class User extends AppModel {
     return $data;
   }
 
-  function comparePassword() {
-    return $this->data['User']['password']
-        == $this->data['User']['password_confirm'];
-  }
-
-  /**
-   * When editing, it's ok to not fill in a password.
-   */
   function beforeValidate() {
-    if (
-      isset($this->data['User']['id']) && empty($this->data['User']['password'])
+    /* When editing their profile, a user might not reenter their password. */
+    if ( isset($this->data['User']['id'])
+      && empty($this->data['User']['password'])
     ) {
+      unset($this->data['User']['password']);
       unset($this->validate['password']);
+      unset($this->validate['password_confirm']);
     }
   }
 
   function beforeSave() {
-    if (
-      isset($this->data['User']['id']) && empty($this->data['User']['password'])
-    ) {
-      $this->data['User']['password'] = $this->field('User.password');
-    } else {
-      $this->data = $this->hashPasswords($this->data, false);
-    }
+    $this->data = $this->hashPasswords($this->data, false);
 
     return true;
   }
 
   function beforeFind($query) {
+    /**
+     * Unfortunately, the auth component doesn't support case-insensitive
+     * usernames. To add support for this, while still allowing a user to
+     * display set their name to a custom case, I need to intercept the queries
+     * for both logging in and checking for an existing user when registering /      * editing.
+     */
     /* Login */
     if (isset($query['conditions']['User.username'])) {
       $query['conditions']['UPPER(User.username)'] =
         strtoupper($query['conditions']['User.username']);
       unset($query['conditions']['User.username']);
     }
-    /* Registration */
+    /* Registration and editing */
     if (isset($query['conditions']['or']['User.username'])) {
       $query['conditions']['or']['UPPER(User.username)'] =
         strtoupper($query['conditions']['or']['User.username']);
