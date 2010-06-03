f7836178c0b4bd85810f24b2061d3bcc814815d9
Ben Heiskell <etherealpanda.mail@gmail.com>
2010-06-03 20:13:05 +0000

    Tons of cosmetic code tweaks to the character models view and controller. Non tested.

--- app/models/character.php
+++ app/models/character.php
@@ -3,16 +3,58 @@ class Character extends AppModel {
 
   var $name = 'Character';
   var $validate = array(
-    'user_id'    => array('required' => false, 'allowEmpty' => false, 'rule' => array('comparison', '>=', 0)),
-    'wallet'     => array('required' => true, 'allowEmpty' => false, 'rule' => array('comparison', '>=', 0)),
-    'rank'       => array('required' => true, 'allowEmpty' => false, 'rule' => array('range', 0, 11)),
-    'race'       => array('required' => true, 'allowEmpty' => false, 'rule' => array('maxlength', 256)),
-    'faction'    => array('required' => true, 'allowEmpty' => false, 'rule' => array('maxlength', 256)),
-    'residency'  => array('required' => true, 'allowEmpty' => false, 'rule' => array('maxlength', 256)),
-    'profession' => array('required' => true, 'allowEmpty' => false, 'rule' => array('maxlength', 256)),
-    'avatar'     => array(
-      'maxlength' => array('rule' => array('maxlength', 1024)),
-      'url'       => array('rule' => 'url', 'allowEmpty' => true)
+    'user_id' => array(
+      'required'     => false,
+      'allowEmpty'   => false,
+      'rule'         => array('comparison', '>=', 0),
+      'message'      => 'Invalid Member'
+    ),
+    'wallet' => array(
+      'required'     => true,
+      'allowEmpty'   => false,
+      'rule'         => array('comparison', '>=', 0),
+      'message'      => 'Wallet must be greater than or equal to zero.'
+    ),
+    'rank' => array(
+      'required'     => true,
+      'allowEmpty'   => false,
+      'rule'         => array('range', 0, 11),
+      'message'      => 'Valid ranks are from one to ten.'
+    ),
+    'race' => array(
+      'required'     => true,
+      'allowEmpty'   => false,
+      'rule'         => array('maxlength', 256),
+      'message'      => 'Race must be less than 256 characters.'
+    ),
+    'faction' => array(
+      'required'     => true,
+      'allowEmpty'   => false,
+      'rule'         => array('maxlength', 256),
+      'message'      => 'Faction must be less than 256 characters.'
+    ),
+    'residency' => array(
+      'required'     => true,
+      'allowEmpty'   => false,
+      'rule'         => array('maxlength', 256),
+      'message'      => 'Residency must be less than 256 characters.'
+    ),
+    'profession' => array(
+      'required'     => true,
+      'allowEmpty'   => false,
+      'rule'         => array('maxlength', 256),
+      'message'      => 'Profession must be less than 256 characters.'
+    ),
+    'avatar' => array(
+      'maxlength' => array(
+        'rule'       => array('maxlength', 1024),
+        'message'    => 'Avatar must be less than 1024 characters.'
+      ),
+      'url' => array(
+        'rule'       => 'url',
+        'allowEmpty' => true,
+        'message'    => 'Avatar must be a valid URL.'
+      )
     )
   );
 
--- app/views/characters/edit.ctp
+++ app/views/characters/edit.ctp
@@ -1,31 +1,21 @@
 <div class="characters form">
 <?php echo $form->create('Character');?>
-	<fieldset>
- 		<legend><?php __('Edit Character');?></legend>
-	<?php
-		echo $form->input('id');
-		echo $form->input('name');
-		echo $form->input('description');
-		echo $form->input('history');
-		echo $form->input('rank');
-		echo $form->input('wallet');
-		echo $form->input('race');
-		echo $form->input('faction');
-		echo $form->input('residency');
-		echo $form->input('profession');
-		echo $form->input('avatar');
-		echo $form->input('is_npc');
-		echo $form->input('is_deactivated');
-		echo $form->input('user_id');
-	?>
-	</fieldset>
+  <fieldset>
+    <legend><?php __('Edit Character');?></legend>
+    <?php
+      echo $form->input('id');
+      echo $form->input('name');
+      echo $form->input('description');
+      echo $form->input('history');
+      echo $form->input('rank');
+      echo $form->input('wallet');
+      echo $form->input('race');
+      echo $form->input('faction');
+      echo $form->input('residency');
+      echo $form->input('profession');
+      echo $form->input('avatar');
+      echo $form->input('is_npc');
+    ?>
+  </fieldset>
 <?php echo $form->end(__('Submit', true));?>
-</div>
-<div class="actions">
-	<ul>
-		<li><?php echo $html->link(__('Delete', true), array('action' => 'delete', $form->value('Character.id')), null, sprintf(__('Are you sure you want to delete # %s?', true), $form->value('Character.id'))); ?></li>
-		<li><?php echo $html->link(__('List Characters', true), array('action' => 'index'));?></li>
-		<li><?php echo $html->link(__('List Users', true), array('controller' => 'users', 'action' => 'index')); ?> </li>
-		<li><?php echo $html->link(__('New User', true), array('controller' => 'users', 'action' => 'add')); ?> </li>
-	</ul>
 </div>
