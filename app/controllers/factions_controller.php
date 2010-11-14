28ed8a3235ee199c8e95d0e406fe54c35955b828
Ben Heiskell <etherealpanda.mail@gmail.com>
2010-11-14 22:45:16 +0000

    Removed unneeded views that were basically cakephp scaffolding baked. This just results in problems when updating these tables. The location is too customized to remove. Also, I'm having an issue with scaffolding and location races because of the likelihood translation and it's binding to the character locations. This is probably a symptom of a greater design issue regarding that model.

--- app/controllers/factions_controller.php
+++ app/controllers/factions_controller.php
@@ -1,67 +1,6 @@
 <?php
 class FactionsController extends AppController {
 
-	var $name = 'Factions';
-	var $helpers = array('Html', 'Form');
-
-	function admin_index() {
-		$this->set('factions', $this->paginate());
-	}
-
-	function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid Faction', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('faction', $this->Faction->read(null, $id));
-	}
-
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->Faction->create();
-			if ($this->Faction->save($this->data)) {
-				$this->Session->setFlash(__('The Faction has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The Faction could not be saved. Please, try again.', true));
-			}
-		}
-		$races = $this->Faction->Race->find('list');
-		$this->set(compact('races'));
-	}
-
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid Faction', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Faction->save($this->data)) {
-				$this->Session->setFlash(__('The Faction has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The Faction could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Faction->read(null, $id);
-		}
-		$races = $this->Faction->Race->find('list');
-		$this->set(compact('races'));
-	}
-
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for Faction', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if ($this->Faction->del($id)) {
-			$this->Session->setFlash(__('Faction deleted', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('The Faction could not be deleted. Please, try again.', true));
-		$this->redirect(array('action' => 'index'));
-	}
-
+  var $name = 'Factions';
+  var $scaffold = 'admin';
 }
-?>
