80267573cdd64594bb712ae77f04a405074d9d3c
Ben Heiskell <etherealpanda.mail@gmail.com>
2010-11-23 23:43:46 +0000

    Added a test for the user rank calculation in conjunction with a bug. Proved the bug is elsewhere at least...

--- app/tests/cases/models/user.test.php
+++ app/tests/cases/models/user.test.php
@@ -4,30 +4,40 @@
 App::import('Model', 'User');
 
 class UserTestCase extends CakeTestCase {
-	var $User = null;
-	var $fixtures = array('app.user');
+  var $User = null;
+  var $fixtures = array('app.user', 'app.entry');
 
-	function startTest() {
-		$this->User =& ClassRegistry::init('User');
-	}
+  function startTest() {
+    $this->User =& ClassRegistry::init('User');
+  }
 
-	function testUserInstance() {
-		$this->assertTrue(is_a($this->User, 'User'));
-	}
+  function testUserInstance() {
+    $this->assertTrue(is_a($this->User, 'User'));
+  }
 
-	function testUserFind() {
-		$this->User->recursive = -1;
-		$results = $this->User->find('first');
-		$this->assertTrue(!empty($results));
+  function testUserFind() {
+    $results = $this->User->find('first');
+    $this->assertTrue(isset($results['User']['rank']));
+  }
 
-		$expected = array('User' => array(
-			'id' => 1,
-			'username' => 'Lorem ipsum dolor sit amet',
-			'password' => 'Lorem ipsum dolor sit amet',
-			'email' => 'Lorem ipsum dolor sit amet',
-			'avatar' => 'Lorem ipsum dolor sit amet'
-		));
-		$this->assertEqual($results, $expected);
-	}
+  function testUserFindRankById() {
+    $result = $this->User->findRankById(1);
+    $this->assertEqual(1, $result);
+
+    $result = $this->User->findRankById(2);
+    $this->assertEqual(2, $result);
+
+    $result = $this->User->findRankById(3);
+    $this->assertEqual(0, $result);
+
+    $result = $this->User->findRankById(4);
+    $this->assertEqual(0, $result);
+
+    $result = $this->User->findRankById(5);
+    $this->assertEqual(7, $result);
+
+    $result = $this->User->findRankById(6);
+    $this->assertEqual(4, $result);
+  }
 }
 ?>
--- app/tests/fixtures/user_fixture.php
+++ app/tests/fixtures/user_fixture.php
@@ -3,6 +3,9 @@ class UserFixture extends CakeTestFixture {
   var $name = 'User';
   var $import = 'User';
   var $records = array(
+    /**
+     * Owns one character who has been in two stories and is active in one.
+     */
     array(
       'id' => '1',
       'username' => 'test_account_1',
@@ -10,9 +13,13 @@ class UserFixture extends CakeTestFixture {
       'email' => 'test_account_1@example.com',
       'url' => 'http://www.example.com',
       'avatar' => 'http://www.example.com/avatar.jpg',
+      'is_admin' => '0',
       'created' => '2010-06-16 19:56:07',
       'modified' => '2010-10-03 21:44:23',
     ),
+    /**
+     * Owns one character who has entered and left a story.
+     */
     array(
       'id' => '2',
       'username' => 'test_account_2',
@@ -20,9 +27,13 @@ class UserFixture extends CakeTestFixture {
       'email' => 'test_account_2@example.com',
       'url' => 'http://www.example.com',
       'avatar' => '',
+      'is_admin' => '0',
       'created' => '2010-06-16 19:56:07',
       'modified' => '2010-10-03 21:44:23',
     ),
+    /**
+     * Owns one character who has entered a story.
+     */
     array(
       'id' => '3',
       'username' => 'test_account_3',
@@ -30,9 +41,13 @@ class UserFixture extends CakeTestFixture {
       'email' => '',
       'url' => '',
       'avatar' => '',
+      'is_admin' => '0',
       'created' => '2010-06-16 19:56:07',
       'modified' => '2010-10-03 21:44:23',
     ),
+    /**
+     * Owns one character who has done nothing.
+     */
     array(
       'id' => '4',
       'username' => 'test_account_4',
@@ -40,6 +55,35 @@ class UserFixture extends CakeTestFixture {
       'email' => '',
       'url' => '',
       'avatar' => '',
+      'is_admin' => '0',
+      'created' => '2010-06-16 19:56:07',
+      'modified' => '2010-10-03 21:44:23',
+    ),
+    /**
+     * Owns no characters. Is an admin.
+     */
+    array(
+      'id' => '5',
+      'username' => 'test_account_5',
+      'password' => 'BOGUS_DATA',
+      'email' => '',
+      'url' => '',
+      'avatar' => '',
+      'is_admin' => '1',
+      'created' => '2010-06-16 19:56:07',
+      'modified' => '2010-10-03 21:44:23',
+    ),
+    /**
+     * Owns no characters. TODO Has a entry offset of 100.
+     */
+    array(
+      'id' => '6',
+      'username' => 'test_account_6',
+      'password' => 'BOGUS_DATA',
+      'email' => '',
+      'url' => '',
+      'avatar' => '',
+      'is_admin' => '1',
       'created' => '2010-06-16 19:56:07',
       'modified' => '2010-10-03 21:44:23',
     ),
