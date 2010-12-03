51252cbe9ec64370ea696513d32aa43e35a29b29
Ben Heiskell <etherealpanda.mail@gmail.com>
2010-12-03 18:49:53 +0000

    Adding a 403 error for when a user is unauthorized to make a request.

--- app/app_error.php
+++ app/app_error.php
@@ -2,6 +2,33 @@
 class AppError extends ErrorHandler {
 
   /**
+   * error403
+   *
+   * Error code for when an unauthorized user hits admin or moderator resources
+   *
+   * @access public
+   * @return void
+   */
+  function error403() {
+    $params = $this->controller->params;
+
+    $this->log(sprintf(
+      __('%s attempted to access a restricted resource %s/%s', true),
+      $this->controller->Auth->user('username'),
+      $params['controller'],
+      $params['action']
+    ));
+
+    header("HTTP/1.1 403 Forbidden");
+    $this->controller->set(array(
+      'name' => __('403 Forbidden', true),
+      'title' => __('403 Forbidden', true),
+    ));
+
+    $this->_outputMessage('error403');
+  }
+
+  /**
    * error500
    *
    * Catch all error handler. Redirecting to a 404 doesn't always make sense.
@@ -16,7 +43,7 @@ class AppError extends ErrorHandler {
     }
     $this->log(Debugger::trace(), 'stacktrace');
 
-    header("HTTP/1.0 500 Internal server error");
+    header("HTTP/1.1 500 Internal server error");
     $this->controller->set(array(
       'name' => __('500 Internal server error', true),
       'title' => __('500 Internal server error', true),
---
+++ app/views/errors/error403.ctp
@@ -0,0 +1,21 @@
+<div class="error403">
+  <h2><?php echo $name; ?></h2>
+  <p>
+    This page is displayed when authorization error has occurred. This can occur
+    when:
+  </p>
+  <ul>
+    <li>Not logged in</li>
+    <li>Accessing an administrative resource</li>
+    <li>Moderating a story or entry without proper access</li>
+    <li>Modify another member's resource, e.g., their character</li>
+    <li>When the project developer made a mistake :-)</li>
+  </ul>
+  <p>
+    If you need moderate access to a story, contact the story's moderator. This
+    is usually the original creator of the story.
+  </p>
+  <p>
+    Otherwise contact an administrator for further assistance.
+  </p>
+</div>
