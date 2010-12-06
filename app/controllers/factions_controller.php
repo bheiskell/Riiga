dd6f1445a75d9995dab21063a3e76b8df8695933
Ben Heiskell <etherealpanda.mail@gmail.com>
2010-12-06 22:00:00 +0000

    Fixing the scaffolding to resolve habtm data. The containable object is automatically applied to all models, so habtm data in scaffolding is not pulled down by default. This results in undoing relations previously set when hitting the edit page. This fix was applied in conjunction with adding images to the locations tags.

--- app/controllers/factions_controller.php
+++ app/controllers/factions_controller.php
@@ -2,4 +2,13 @@
 class FactionsController extends AppController {
   var $name = 'Factions';
   var $scaffold = 'admin';
+
+  function beforeFilter() {
+    parent::beforeFilter();
+
+    // Edit page needs to contain the races in the HABTM relation
+    if ('admin_edit' == $this->action) {
+      $this->Faction->contain('Race');
+    }
+  }
 }
--- app/controllers/location_tags_controller.php
+++ app/controllers/location_tags_controller.php
@@ -2,4 +2,13 @@
 class LocationTagsController extends AppController {
   var $name = 'LocationTags';
   var $scaffold = 'admin';
+
+  function beforeFilter() {
+    parent::beforeFilter();
+
+    // Edit page needs to contain the locations in the HABTM relation
+    if ('admin_edit' == $this->action) {
+      $this->LocationTag->contain('Location');
+    }
+  }
 }
